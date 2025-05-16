83e234380694827426e7bae922bf7847a2bda0b8
Park Changyeong <amuileum07@gmail.com>
2025-05-16 16:54:15 +0900

    방향 수정

--- Assets/HandTracking.cs
+++ Assets/HandTracking.cs
@@ -11,6 +11,9 @@ public class HandTracking : MonoBehaviour
 
     private int offset = 70;
 
+    private Vector3 cameraTransform = new Vector3(6, 5, -10);
+    
+
     void Start()
     {
         leftHandPoints = new GameObject[21];
@@ -45,18 +48,22 @@ public class HandTracking : MonoBehaviour
     void Update()
     {
         HandData handData = udpReceive.handData;
+        Quaternion handRotation = Quaternion.Euler(180f, 0f, 180f);
 
         // 왼손 처리
         if (handData != null && handData.Left != null && handData.Left.Length >= 63)
         {
             for (int i = 0; i < 21; i++)
             {
-                float x = 7 - handData.Left[i * 3] / offset;
-                float y = handData.Left[i * 3 + 1] / offset;
-                float z = handData.Left[i * 3 + 2] / offset;
+                float x = -(7 - handData.Left[i * 3] / offset );
+                float y = (handData.Left[i * 3 + 1] / offset );
+                float z = (handData.Left[i * 3 + 2] / offset );
+
+                Vector3 rawPos = new Vector3(x, y, z);
+                Vector3 rotatedPos = handRotation * rawPos;
 
                 if (leftHandPoints != null && i < leftHandPoints.Length && leftHandPoints[i] != null)
-                    leftHandPoints[i].transform.localPosition = new Vector3(x, y, z);
+                    leftHandPoints[i].transform.localPosition = rotatedPos;
             }
         }
 
@@ -65,12 +72,15 @@ public class HandTracking : MonoBehaviour
         {
             for (int i = 0; i < 21; i++)
             {
-                float x = 7 - handData.Right[i * 3] / offset;
-                float y = handData.Right[i * 3 + 1] / offset;
-                float z = handData.Right[i * 3 + 2] / offset;
+                float x = -(7 - handData.Right[i * 3] / offset);
+                float y = (handData.Right[i * 3 + 1] / offset);
+                float z = (handData.Right[i * 3 + 2] / offset);
+
+                Vector3 rawPos = new Vector3(x, y, z);
+                Vector3 rotatedPos = handRotation * rawPos;
 
                 if (rightHandPoints != null && i < rightHandPoints.Length && rightHandPoints[i] != null)
-                    rightHandPoints[i].transform.localPosition = new Vector3(x, y, z);
+                    rightHandPoints[i].transform.localPosition = rotatedPos;
             }
         }
 
